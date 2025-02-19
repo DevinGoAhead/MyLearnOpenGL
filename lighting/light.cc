319ae62f8c93fa200f4b43cdc8e41a2ea443472c
DevinWei <wxyback@outlook.com>
2025-02-20 00:21:50 +0800

    Implement a dynamic light with Phong Reflectance Model

--- lighting/light.cc
+++ lighting/light.cc
@@ -31,6 +31,11 @@ int main()
 	
 	GLuint VBO;
 	glGenBuffers(1, &VBO); // 创建 vertexBuffer, 绑定到 ID VBO 上
+
+	// 创建纹理对象
+	GLuint texture0;
+	glGenTextures(1, &texture0); // 创建一个纹理对象, 将其 ID 存储在 texture0 中
+
 	// boxVAO
 	GLuint boxVAO;
 	glGenVertexArrays(1, &boxVAO); // 创建 vertexArray, 绑定到 ID boxVAO 上
@@ -39,13 +44,47 @@ int main()
 	
 	// 在 GPU 中开辟指定类型的缓冲区, 用于存放 vertices indeies
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glActiveTexture(GL_TEXTURE0); // 激活纹理单元0, 并绑定 texture0
+	glBindTexture(GL_TEXTURE_2D, texture0);
+	// 设置纹理参数
+	// 分别设置 ST 方向的环绕方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// 设置纹理映射缩放算法
+	// 当纹理被放大, 线性插值, 避免像素化
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
+	// 当纹理被缩小, 使用 MipMap 算法, 且采用三线性插值的算法
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); 
+
+	// 加载图像
+	stbi_set_flip_vertically_on_load(true); // 加载图像时反转 y 轴
+	int texWidth, texHeight, nRChannels;
+	unsigned char* pImageData = stbi_load("./model/container.jpg", &texWidth, &texHeight, &nRChannels, 0);
+	if(!pImageData) 
+	{
+		std::cerr << "failed to load Image" << '\n';
+		exit(1);
+	}
+
+	// 生成纹理图像
+	// 生成的纹理图像将保存在 glGenTextures 所生成的对象中
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, pImageData);
+	glGenerateMipmap(GL_TEXTURE_2D);// 自动生成多级渐远纹理
+	stbi_image_free(pImageData);// 释放图片资源
 	
 	// 顶点着色器 location index, 属性元素分量, 类型, 是否标准化, 步距, 相对起点偏移
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0); // 顶点坐标
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat))); // 法线坐标(向量)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat))); // 纹理
 	glEnableVertexAttribArray(0); // 启用location 0
+	glEnableVertexAttribArray(1); // 启用location 1
+	glEnableVertexAttribArray(2); // 启用location 2
 	glBindVertexArray(0); // 将 boxVAO 从 OpenGL 当前上下文解绑
 
-	//lightVAO, 但是我觉得在当前练习中, 使用不同的 VAO 没有任何意义
+	// lightVAO
+	// 本例使用的是点光源, 但是使用了 box 作为壳, 使光源能够被观察到
 	GLuint lightVAO;
 	glGenVertexArrays(1, &lightVAO); // 创建 vertexArray, 绑定到 ID boxVAO 上
 	glBindVertexArray(lightVAO); // 绑定 boxVAO 到 openGL 上下文, 开始记录信息
@@ -54,7 +93,7 @@ int main()
 	//这里不需要再次在 GPU 中开辟内存了
 	
 	// 顶点着色器 location index, 属性元素分量, 类型, 是否标准化, 步距, 相对起点偏移
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
 	glEnableVertexAttribArray(0); // 启用 location 0
 	glBindVertexArray(0); // 将 lightVAO 从 OpenGL 当前上下文解绑
 	
@@ -71,35 +110,44 @@ int main()
 	glfwSwapInterval(1); // 前后缓冲区交换间隔
 	while(!glfwWindowShouldClose(pWindow))
 	{
-		glClearColor(0.1f, 0.3f, 0.5f, 1.0f);//设置清除颜色缓冲区后要使用的颜色-纯色
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);//设置清除颜色缓冲区后要使用的颜色-纯色
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 清楚颜色缓冲区和深度缓冲区
 
 		curTime = glfwGetTime();
 		perFrameTime = curTime - lastTime;
 		lastTime = curTime;
+
+		glm::vec3 lightPos{3.f * glm::sin(curTime), 0.f, 3.f * glm::cos(curTime)}; //沿半径为3 的圆周运动
 		// 视图变换和投影变换矩阵
 		glm::mat4 view = camera.GetViewMatrix();
 		glm::mat4 project = glm::perspective(glm::radians(camera.GetFov()), (float)wndWidth / (float)wndHeight, 0.1f, 100.f); 
 		//draw box
 		boxShaderProgram.Use();
 		glBindVertexArray(boxVAO);
-		glm::mat4 boxModel = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -5.f));
-		boxModel = glm::rotate(boxModel, glm::radians(45.f), glm::vec3(0.f, 0.f, 1.f));
+		glm::mat4 boxModel = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -1.f)); // 3 向 -z 方向移动一下, 确保观察大小合适
+		boxModel = glm::rotate(boxModel, 10 * glm::radians(curTime), glm::vec3(0.f, 1.f, 0.f)); // 2. 随时间沿 y 轴旋转
+		boxModel = glm::rotate(boxModel, glm::radians(20.f), glm::vec3(1.f, 0.f, 0.f)); //1. 沿 x 轴旋转一个小的角度, 可以看到顶面
 		glUniformMatrix4fv(glGetUniformLocation(boxShaderProgram.ID(), "model_"), 1, GL_FALSE, glm::value_ptr(boxModel));
 		glUniformMatrix4fv(glGetUniformLocation(boxShaderProgram.ID(), "view_"), 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(glGetUniformLocation(boxShaderProgram.ID(), "project_"), 1, GL_FALSE, glm::value_ptr(project));
 		glUniform3fv(glGetUniformLocation(boxShaderProgram.ID(), "boxColor_"), 1, glm::value_ptr(boxColor));
 		glUniform3fv(glGetUniformLocation(boxShaderProgram.ID(), "lightColor_"), 1, glm::value_ptr(lightColor));
+		glUniform3fv(glGetUniformLocation(boxShaderProgram.ID(), "lightPos_"), 1, glm::value_ptr(lightPos));
+		boxShaderProgram.SetUniform("texturer0_", 0);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// draw light
 		lightShaderProgram.Use();
 		glBindVertexArray(lightVAO);
-		glm::mat4 lightModel = glm::translate(glm::mat4(1.f), glm::vec3(15.f, 4.f, -40.f)); // 将光源向远处, 右上角移动
+		
+		glm::mat4 lightModel = glm::translate(glm::mat4(1.f), lightPos);// 3. 沿半径为5 的圆周运动
+		lightModel = glm::translate(lightModel, glm::vec3(0.f, 0.f, -1.f)); // 2. 移动到和box 在同一个深度上
+		lightModel = glm::scale(lightModel, glm::vec3(0.2f, 0.2f, 0.2f));// 1. 适当缩放, 使其能观察到, 不至于太大
 		glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram.ID(), "model_"), 1, GL_FALSE, glm::value_ptr(lightModel));
 		glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram.ID(), "view_"), 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(glGetUniformLocation(lightShaderProgram.ID(), "project_"), 1, GL_FALSE, glm::value_ptr(project));
 		glUniform3fv(glGetUniformLocation(lightShaderProgram.ID(), "lightColor_"), 1, glm::value_ptr(lightColor));
+		glUniform3fv(glGetUniformLocation(lightShaderProgram.ID(), "cameraPos_"), 1, glm::value_ptr(camera.GetPos()));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		
 		// 渲染
--- lighting/shader/boxVS.glsl
+++ lighting/shader/boxVS.glsl
@@ -3,13 +3,27 @@
 // in 表示该数据需要从外部输入
 // vec3 为变量类型, position 为变量名称
 // 在 glVertexAttribPointer 函数中配置该位置，将 location的ID 告知 GPU, GPU 解析数据后将存到 posiotion 中
-layout (location = 0) in vec3 position_v;
+layout (location = 0) in vec3 vertex_v;
+layout (location = 1) in vec3 normal_v;
+layout (location = 2) in vec2 verTexCoords_v;
 
 uniform mat4 model_;
 uniform mat4 view_;
 uniform mat4 project_;
 
+out vec3 _normal_;
+out vec3 _vertex_;
+out vec2 _verTexCoords_;
+
+
 void main()
 {
-	gl_Position = project_ * view_ * model_ * vec4(position_v, 1.0f);// 转换为齐次坐标
+	gl_Position = project_ * view_ * model_ * vec4(vertex_v, 1.0f);// 转换为齐次坐标
+	// 本例在世界空间中进行渲染, 故法线和顶点都传递到世界空间在 片段着色器中渲染
+	// 法线矩阵要用逆转置, 才能确保和面(或其他向量)保持垂直
+	// 强转为 3 * 3 矩阵再于 normal_v 相乘
+	_normal_ =  mat3(transpose(inverse(model_))) * normal_v;
+	_normal_ = normalize(_normal_); // 单位化
+	_vertex_ = vec3(model_ * (vertex_v, 1.f));
+	_verTexCoords_ = verTexCoords_v;
 }
