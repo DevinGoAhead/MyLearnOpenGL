48834ea5e3000098fc4089390976157fac16af54
Devin <dv_back@outlook.com>
2025-04-13 10:21:58 +0800

    add mesh-based bones import functionality

--- MyClass/mesh.hpp
+++ MyClass/mesh.hpp
@@ -1,37 +1,109 @@
 // c++ 的结构体的内存分布是连续的
 
-#ifndef __MESH
-#define __MESH
+#ifndef _MESH
+#define _MESH
 
 #include "../MyClass/shaderProgram.hpp"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include <glm/gtx/string_cast.hpp>
 
 #include "glad/glad.h"
 #include "glfw/glfw3.h"
 
 #include <string>
 #include <vector>
+#include <array>
+#include <utility>
+#include <algorithm>
+
+const uint MaxInfluenceBone = 4;
+
+// 临时存储与顶点关联的骨骼信息, 经过处理后拷贝到 Vertex 中
+class BoneInfluenceSet {
+public:
+	void SortByWeight();
+	void NormalizeWeights();
+	uint Size();
+	void Resize(uint newSize);
+	void PushBack(uint boneID, float weight);
+	void CopyToArray(std::array<int, MaxInfluenceBone> &boneIDs, std::array<float, MaxInfluenceBone> &weights);
+private:
+	std::vector<std::pair<uint, float>> _weightsPerBone; // ID, weight
+};
+
+//BoneWeight(uint vertexID, uint boneID, float weight) : _vertexID(vertexID),  {}
+// 按权重由大到小排序
+void BoneInfluenceSet::SortByWeight() {
+	std::sort(_weightsPerBone.begin(), _weightsPerBone.end(),
+			[](std::pair<uint, float> _boneIDweightLt, std::pair<uint, float> _boneIDweightRt)->bool {
+				return _boneIDweightLt.second > _boneIDweightRt.second;});
+}
+
+// 本例假设每个顶点最多受到 MaxInfluenceBone 个骨骼的影响
+// 如果超过 MaxInfluenceBone 个, 则截断, 这种情况这导致 _weights 之和 < 1, 针对这种情况做归一化
+// 在外部判断 _weightsPerBone.size > MaxInfluenceBone? 同时resize(MaxInfluenceBone), 再调用  NormalizeWeights()
+void BoneInfluenceSet::NormalizeWeights() {
+	float totalWeight = 0.f;
+	for(const auto& _boneIDweight : _weightsPerBone) {totalWeight += _boneIDweight.second;}
+	if(totalWeight <= 0.f ) {
+		if(_weightsPerBone.empty()) {std::cout << "No bones influence current vertex !" << std::endl;}
+		else {std::cout << "Bones' weights are wrong !" << std::endl;}
+	}
+	else {
+		for(auto& _boneIDweight : _weightsPerBone) {
+			_boneIDweight.second /=  totalWeight;
+		}
+	}
+}
+
+uint BoneInfluenceSet::Size() {return _weightsPerBone.size();}
+void BoneInfluenceSet:: Resize(uint newSize) {_weightsPerBone.resize(newSize);}
+void BoneInfluenceSet::PushBack(uint boneID, float weight) {_weightsPerBone.emplace_back(boneID, weight);}
+void BoneInfluenceSet::CopyToArray(std::array<int, MaxInfluenceBone> &boneIDs, std::array<float, MaxInfluenceBone> &weights) {
+	uint size = _weightsPerBone.size(); // 一定 < MaxInfluenceBone
+	for(uint i = 0; i < MaxInfluenceBone; ++i){
+		if(i < size) {
+			boneIDs[i] = _weightsPerBone[i].first;
+			weights[i] = _weightsPerBone[i].second;
+		}
+		else {
+			boneIDs[i] = -1;
+			weights[i] = 0.f;
+		}
+	}
+}
+
+// 顶点
+struct Vertex{
+	glm::vec3 _position;
+	glm::vec3 _normal;
+	glm::vec2 _texCoord;
+	glm::vec3 _tangent;
+	std::array<int, MaxInfluenceBone> _boneIDs;
+	std::array<float, MaxInfluenceBone> _weights;
+
+	//LearnOpenGL 的逻辑
+	// void SetBoneIDs_Weights(int boneID, float weight){
+	// 	for(uint i = 0; i < MaxInfluenceBone; ++i) {
+	// 		if(_boneIDs[i] != -1) {
+	// 			_boneIDs[i] = boneID;
+	// 			_weights[i] = weight;
+	// 		}
+	// 	}
+	// }
+};
+
+// 纹理
+struct Texture{
+	uint _id; // 纹理ID
+	std::string _typeName; // 纹理类型
+	std::string _fileName; // 文件名
+};
 
 namespace wxy{
-	using uint = unsigned int;
-	// 顶点
-	struct Vertex{
-		glm::vec3 _position;
-		glm::vec3 _normal;
-		glm::vec2 _texCoord;
-		glm::vec3 _tangent;
-	};
-
-	// 纹理
-	struct Texture{
-		uint _id; // 纹理ID
-		std::string _typeName; // 纹理类型
-		std::string _fileName; // 文件名
-	};
-
 	// Mesh
 	class Mesh{
 	public:
@@ -102,9 +174,14 @@ namespace wxy{
 		// 告知 GPU 如何解析顶点数据
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _position)); // 位置坐标
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _normal)); // 法线坐标
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _texCoord)); // 纹理坐标
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _texCoord)); // 材质纹理
 		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _tangent)); // 法线纹理
-		glEnableVertexAttribArray(0), glEnableVertexAttribArray(1), glEnableVertexAttribArray(2), glEnableVertexAttribArray(3);;
+		
+		glVertexAttribIPointer(4, MaxInfluenceBone, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, _boneIDs)); // 影响骨骼的 ID 数组, 分量 4
+		glVertexAttribPointer(5, MaxInfluenceBone, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, _weights)); // 影响骨骼的 weight 数组, 分量 4
+		
+		glEnableVertexAttribArray(0); glEnableVertexAttribArray(1); glEnableVertexAttribArray(2); glEnableVertexAttribArray(3);
+		glEnableVertexAttribArray(4); glEnableVertexAttribArray(5);
 
 		glBindVertexArray(0);// 解绑 _VAO, 停止记录
 	}
--- MyClass/model.hpp
+++ MyClass/model.hpp
@@ -1,40 +1,59 @@
-#ifndef __MODEL
-#define __MODEL
+#ifndef _MODEL
+#define _MODEL
 
+#ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h" // 加载纹理图像
+#endif
 
 #include "assimp/Importer.hpp"
 #include "assimp/scene.h"
 #include "assimp/postprocess.h"
 
 #include "mesh.hpp"
+#include "assimpGLMHelpers.hpp"
 #include <iostream>
 #include <map>
 #include <cstring>
+#include <type_traits>
+
+struct BoneInfo {
+	BoneInfo(int id = -1, glm::mat4 offset = glm::mat4(1.f))
+	: _id(id), _offset(offset) {}
+
+	int _id; // bone 的下标(索引)
+	glm::mat4 _offset; // 从骨骼空间到模型空间
+};
 
 namespace wxy {
 	class Model {
 	public:
 		Model(const std::string& pathName);
 		void Draw(wxy::ShaderProgram shaderProgram);
+		std::map<std::string, BoneInfo>& GetBoneInfoMap();
+		uint& GetBoneCount();
 		//~Model();
 	private:
 		void LoadModel(const std::string& pathName); // 加载模型, pathName 模型文件的路径
 		void ProcessNode(aiNode* assimpNode, const aiScene* assimpScene); // 遍历所有节点 及其 子节点
 		Mesh ProcessMesh(aiMesh* assimpMesh, const aiScene* assimpScene);
 		std::vector<Texture> loadMaterialTextures(aiMaterial* assimpMat, aiTextureType assimpTexType, const std::string& texTypeName);
+		//void LoadMeshBones(std::vector<Vertex>& vertices, aiMesh* assimpMesh, const aiScene* assimpScene);
+		void LoadMeshBones(std::vector<BoneInfluenceSet>& boneIDweights, aiMesh* assimpMesh, const aiScene* assimpScene);
 		uint TextureFromFile(const std::string& assimpTexFileName/*, const std::string& texTypeName*/);
 	private:
 		std::vector<Mesh> _meshes; // 存放加载的所有mesh
-		std::map<std::string, Texture> _texturesLoaded; // 已经加载过的模型
+		std::map<std::string, Texture> _texturesLoaded; // 已经加载过的纹理, string 是纹理的名称
+		std::map<std::string, BoneInfo> _boneInfoMap; // 已经加载过的骨骼, string 是骨骼的名称
+		uint _boneCount; //所有 mesh, 累计加载的骨骼的数量
 		std::string _directory; // 不含文件名的模型文件的路径
 	};
 
 	//public
-	Model::Model(const std::string& pathName) {
+	Model::Model(const std::string& pathName) : _boneCount(0) {
 		LoadModel(pathName);
 	}
+
 	void Model::Draw(ShaderProgram shaderProgram) {
 		uint size = _meshes.size();
 		//std::cout << size << std::endl;
@@ -42,18 +61,22 @@ namespace wxy {
 			_meshes[i].Draw(shaderProgram);
 		}
 	}
+
+	std::map<std::string, BoneInfo>& Model::GetBoneInfoMap() {return _boneInfoMap;}
+	uint& Model::GetBoneCount() {return _boneCount;}
+
 	//private
-	void Model::LoadModel(const std::string& pathName) {
+	void Model::LoadModel(const std::string& absolutePathName) {
 		Assimp::Importer import;
 		// 将非三角形处理为三角形 | 反转纹理图像的 y 坐标 | 强制统一为 CCW 顺序 | 自动计算 BT
-		const aiScene *assimpScene = import.ReadFile(pathName, 
+		const aiScene *assimpScene = import.ReadFile(absolutePathName, 
 				aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_FlipWindingOrder | aiProcess_CalcTangentSpace); 
 		// 只要有一个为假即判断为失败
 		if(!assimpScene || (assimpScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !assimpScene->mRootNode)) {
 			std::cerr << "ASSIMP::ERROR::" << import.GetErrorString() << std::endl;
 		}
 		
-		_directory = pathName.substr(0, pathName.find_last_of('/')); // 使用 [0, 最后一个/) 之间的内容创建一个子字符串, 其实就是不含文件名的路径
+		_directory = absolutePathName.substr(0, absolutePathName.find_last_of('/')); // 使用 [0, 最后一个/) 之间的内容创建一个子字符串, 其实就是不含文件名的路径
 		ProcessNode(assimpScene->mRootNode, assimpScene);
 	}
 
@@ -103,8 +126,29 @@ namespace wxy {
 			vec3Temp.y = assimpMesh->mTangents[i].y;
 			vec3Temp.z = assimpMesh->mTangents[i].z;
 			vertex._tangent = vec3Temp;
+			
+			// boneIDs 和 boneWeights 的初始化放在了 class BoneInfluenceSet 中
+
 			vertices.push_back(vertex);
 		}
+
+		// 所有顶点初始化完成, 顶点ID 已确定
+
+		std::vector<BoneInfluenceSet> BoneInfluenceSet; // 临时存储骨骼id 和 weight, 与 vertices 一一对应
+		uint sizeVertices = vertices.size();
+		BoneInfluenceSet.resize(sizeVertices);
+		
+		LoadMeshBones(BoneInfluenceSet, assimpMesh, assimpScene);
+		for(uint i = 0; i < sizeVertices; ++i) {
+			auto& boneInfluence = BoneInfluenceSet[i];
+			if(boneInfluence.Size() > MaxInfluenceBone) {
+				boneInfluence.SortByWeight();
+				boneInfluence.Resize(MaxInfluenceBone);
+				boneInfluence.NormalizeWeights();
+			}
+			boneInfluence.CopyToArray(vertices[i]._boneIDs, vertices[i]._weights);
+		}
+
 		// 每个 mesh 会有多个索引, 其实就是顶点索引, 指定了由哪几个点组成当前face
 		uint numFaces = assimpMesh->mNumFaces;
 		for(uint i = 0; i < numFaces; ++i) {
@@ -158,6 +202,34 @@ namespace wxy {
 		}
 
 		return textures;
+	}
+
+	void Model::LoadMeshBones(std::vector<BoneInfluenceSet>& boneIDweights, aiMesh* assimpMesh, const aiScene* assimpScene) {
+		uint numBones = assimpMesh->mNumBones;
+
+		for(uint iBone = 0; iBone < numBones; ++iBone) {
+			// 将骨骼id 和 offset 加载到 _bonesLoaded 中
+			auto bone = assimpMesh->mBones[iBone];
+			std::string boneName = bone->mName.C_Str();
+			if(_boneInfoMap.find(boneName) ==_boneInfoMap.end()) { // 没找到
+				_boneInfoMap[boneName] = BoneInfo(_boneCount++, AssimpGLMHelpers::ConvertMat4ToGLM_Mat4(bone->mOffsetMatrix));
+			}
+
+			// 将骨骼id 和 权重写入到 vetex 中, 每一个骨骼可能会影响多个顶点, 因此一个骨骼要写入到多个顶点中
+			// _bonesLoaded[boneName].boneID, 与 vertex.boneIDs[] 中的元素id 和 vertex.weights[] 一一对应
+			// 只有第一个mesh 中 iBone 和 boneID 相等, 其它 mesh 的 iBone 会从 0 开始, 但 boneID 会累计变化
+			uint boneID = _boneInfoMap[boneName]._id;
+			uint numWeights = bone->mNumWeights;
+			for(uint iWeight = 0; iWeight < numWeights; ++iWeight) {
+				auto vertexWeight = bone->mWeights[iWeight];
+				uint vertexID = vertexWeight.mVertexId;
+				if(vertexID >= boneIDweights.size()) {
+					std::cout << "vertexID >= vertices.size !" << std::endl;
+					assert(NULL);
+				}
+				boneIDweights[vertexID].PushBack(boneID, vertexWeight.mWeight);
+			}
+		}
 	}
 
 	uint Model::TextureFromFile(const std::string& assimpTexFileName/*, const std::string& texTypeName*/) {
